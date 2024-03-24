c26ffb82a12a8801057a22403202d890ad3da687
ozimko93 <ozimko93@gmail.com>
2024-03-24 11:26:43 +0200

    add Tasks 2 and 3

---
+++ index.js
@@ -0,0 +1,16 @@
+function addNotification() {
+  console.log('Я учу JavaScript!');
+  return;
+}
+
+addNotification();
+
+const image = document.getElementById('image');
+console.log(image);
+
+function showNextImage() {
+  image.src = '/image2.png';
+}
+function showPrevImage() {
+  image.src = '/2136.jpg';
+}
